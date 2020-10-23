7b3b0b08b1cbcae9e04641b8375c9455cbbff8df
Aleksander Shuplyk <santurionley@rambler.ru>
2020-10-23 11:26:00 +0300

    Initial Commit

---
+++ Somethig about me/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Somethig about me
+//
+//  Created by Александр Шуплык on 23.10.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
